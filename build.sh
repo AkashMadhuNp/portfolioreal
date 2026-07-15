07086414b7d15a4129dfd4bc6a8ae02bce52fde2
Blyaax <150952601+Blyaax@users.noreply.github.com>
2026-07-15 23:42:03 +0530

    Configure Vercel build

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "Installing Flutter..."
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable
+
+export PATH="$PWD/flutter/bin:$PATH"
+
+flutter doctor
+
+flutter pub get
+
+flutter build web --release
